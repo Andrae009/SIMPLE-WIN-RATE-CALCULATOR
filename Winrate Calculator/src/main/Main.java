63067823e99d82ddd3ac2675878da76895f13f92
Andrae Genesis Valin <109593286+Andrae009@users.noreply.github.com>
2025-04-16 15:48:12 +0800

    V1.0

---
+++ Winrate Calculator/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+import panels.Dashboard;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author andra
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Dashboard().setVisible(true);
+    }
+    
+}
